a5a0fecd0a6ee1de632b5127703f010f64009b8c
Blakjack54 <blakewalters53@gmail.com>
2024-07-11 17:44:18 -0400

    Added Ternary

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,12 @@
 ﻿int answer = 4;
-string response;
-if (answer < 9) 
-{
-    response = answer + " is less than nine";
-}
-else
-{
-    response = answer + " is greater than or equal to nine";
-}
+// string response;
+// if (answer < 9) 
+// {
+//     response = answer + " is less than nine";
+// }
+// else
+// {
+//     response = answer + " is greater than or equal to nine";
+// }
+var response = (answer<9) ? "The answer is less then 9": "The answer is greater then 9";
+Console.WriteLine (response);
